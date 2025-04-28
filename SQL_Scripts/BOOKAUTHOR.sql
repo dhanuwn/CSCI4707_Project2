f9ec417abffb62a890c22572f3291916fd0e17a2
Younus <younus_ali45@icloud.com>
2025-04-28 17:52:37 -0500

    Delete SQL_Scripts/BOOKAUTHOR.sql

--- SQL_Scripts/BOOKAUTHOR.sql
+++
@@ -1,8 +0,0 @@
--- BOOKAUTHOR.sql
-CREATE TABLE BookAuthor (
-    BookAuthorID INT PRIMARY KEY,
-    BookID INT,
-    AuthorID INT,
-    FOREIGN KEY (BookID) REFERENCES Books(BookID),
-    FOREIGN KEY (AuthorID) REFERENCES Authors(AuthorID)
-);
