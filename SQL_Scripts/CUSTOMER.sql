bff65e1c737555d77d3af9b2a9939dde0d45b140
Younus <younus_ali45@icloud.com>
2025-04-28 17:52:45 -0500

    Delete SQL_Scripts/CUSTOMER.sql

--- SQL_Scripts/CUSTOMER.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE Customer (
-    CustomerID INT PRIMARY KEY,
-    Email VARCHAR(255) UNIQUE NOT NULL,
-    Phone_Number VARCHAR(20),
-    First_Name VARCHAR(50),
-    Last_Name VARCHAR(50)
-); 
