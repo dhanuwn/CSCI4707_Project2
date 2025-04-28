5e70f22078d9cc021ef89779b474c0643331172f
Younus <younus_ali45@icloud.com>
2025-04-28 17:52:24 -0500

    Delete SQL_Scripts/OrderDetails.sql

--- SQL_Scripts/OrderDetails.sql
+++
@@ -1,9 +0,0 @@
--- ORDERDETAILS.sql
-CREATE TABLE OrderDetails (
-    OrderID INT,
-    BookID INT,
-    Quantity INT,
-    PRIMARY KEY (OrderID, BookID),
-    FOREIGN KEY (OrderID) REFERENCES Orders(OrderID),
-    FOREIGN KEY (BookID) REFERENCES Books(BookID)
-);
